d02183ac8694f247f883c66668f62c7aab8210cb
434huzaifa <saadhuzaifa2497@gmail.com>
2024-01-18 10:11:09 +0600

    admin user create user views ready

--- index.js
+++ index.js
@@ -3,5 +3,24 @@ const path=require('path')
 
 const app = express() // express function
 const publicPath=path.join(__dirname,'public')
-app.set('view engine','ejs')
 app.use(express.static(publicPath));
+app.set('view engine','ejs')
+
+app.get("",(_,res)=>{
+    res.render("home")
+})
+
+app.get("/adminlogin",(_,res)=>{
+    res.render("login",{isadmin:true})
+})
+app.get("/userlogin",(_,res)=>{
+    res.render("login",{isadmin:false})
+})
+
+app.get("/createuser",(_,res)=>{
+    res.render("createuser")
+})
+
+
+
+app.listen(5000)
