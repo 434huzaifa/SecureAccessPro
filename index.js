8842dbc8a92913085eef3dc184e57eea047ec640
434huzaifa <saadhuzaifa2497@gmail.com>
2024-01-18 10:19:23 +0600

    mongoose setup done

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+require('dotenv').config();
 const express=require('express')
 const path=require('path')
-
-const app = express() // express function
+const mongoose = require('mongoose');
+const app = express() 
 const publicPath=path.join(__dirname,'public')
 app.use(express.static(publicPath));
+app.use(express.json());
 app.set('view engine','ejs')
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@saaddb.bmj48ga.mongodb.net/SecureAccessPro?retryWrites=true&w=majority`
+mongoose.connect(uri)
+
+
+async function run() {
+    try {
+        
+    } catch (error) {
+        console.log(`The Error is:${e.message}`);
+        return
+    }
+}
+run().catch(console.dir);
+
 
 app.get("",(_,res)=>{
     res.render("home")
 })
-
 app.get("/adminlogin",(_,res)=>{
     res.render("login",{isadmin:true})
 })
